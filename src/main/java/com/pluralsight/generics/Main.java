6d833367a68ec563ed7f987d2e8cf9b361fb3d92
Jeff Wong <v-jwong1@expedia.com>
2020-02-27 19:34:16 +1000

    main function of generics - stop run time types error in compile time

--- src/main/java/com/pluralsight/generics/Main.java
+++ src/main/java/com/pluralsight/generics/Main.java
@@ -1,4 +1,16 @@
 package com.pluralsight.generics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("c");
+        list.add("a");
+        list.add("b");
+
+        String string1 = list.get(0);
+        System.out.println(string1);
+    }
 }
