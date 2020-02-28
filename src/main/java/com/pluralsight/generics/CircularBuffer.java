1d460c6dd36cd66bb807e445f24fbb31feab2bfe
Jeff Wong <v-jwong1@expedia.com>
2020-02-28 17:28:57 +1000

    stub out CircularBuffer class methods

--- src/main/java/com/pluralsight/generics/CircularBuffer.java
+++ src/main/java/com/pluralsight/generics/CircularBuffer.java
@@ -1,4 +1,36 @@
 package com.pluralsight.generics;
 
 public class CircularBuffer {
+    private Object[] objects;
+    private int readCursor = 0;
+    private int writeCursor = 0;
+
+    public CircularBuffer(int size) {
+        this.objects = new Object[size];
+    }
+
+    public boolean offer(Object obj) {
+        if (objects[writeCursor] != null) {
+            return false;
+        }
+
+        objects[writeCursor] = obj;
+        writeCursor = next(writeCursor);
+        return true;
+    }
+
+    public Object poll() {
+        Object currentObject = objects[readCursor];
+
+        if (currentObject != null) {
+            objects[readCursor] = null;
+            readCursor = next(readCursor);
+        }
+
+        return currentObject;
+    }
+
+    private int next(int index) {
+        return (index + 1) % objects.length;
+    }
 }
