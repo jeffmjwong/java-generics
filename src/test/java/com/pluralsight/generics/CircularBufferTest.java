77cd0e69ebf487bd8a27d55f430ad80b98297bd0
Jeff Wong <v-jwong1@expedia.com>
2020-02-28 17:29:31 +1000

    create some test cases

--- src/test/java/com/pluralsight/generics/CircularBufferTest.java
+++ src/test/java/com/pluralsight/generics/CircularBufferTest.java
@@ -1,4 +1,26 @@
 package com.pluralsight.generics;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
 public class CircularBufferTest {
+    private CircularBuffer buffer = new CircularBuffer(2);
+
+    @Test
+    public void shouldOfferPollableElement() {
+        assertTrue(buffer.offer(1));
+        assertEquals(1, buffer.poll());
+        assertNull(buffer.poll());
+    }
+
+    @Test
+    public void shouldNotOfferWhenBufferIsFull() {
+        assertTrue(buffer.offer(1));
+        assertTrue(buffer.offer(2));
+        assertFalse(buffer.offer(3));
+    }
 }
