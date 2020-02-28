e1ab69c8454a01f0f31068b60c69d9be61acd0f6
Jeff Wong <v-jwong1@expedia.com>
2020-02-28 17:32:20 +1000

    create shouldRecycleBuffer test case

--- src/test/java/com/pluralsight/generics/CircularBufferTest.java
+++ src/test/java/com/pluralsight/generics/CircularBufferTest.java
@@ -28,4 +28,14 @@ public class CircularBufferTest {
     public void shouldNotPollWhenBufferIsEmpty() {
         assertNull(buffer.poll());
     }
+
+    @Test
+    public void shouldRecycleBuffer() {
+        assertTrue(buffer.offer(1));
+        assertTrue(buffer.offer(2));
+        assertEquals(1, buffer.poll());
+        assertTrue(buffer.offer(3));
+        assertEquals(2, buffer.poll());
+        assertEquals(3, buffer.poll());
+    }
 }
