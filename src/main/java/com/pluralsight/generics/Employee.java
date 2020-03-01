630384d131687088b39bdc3d7e05958a05a8fab4
Jeff Wong <v-jwong1@expedia.com>
2020-03-01 16:58:47 +1000

    complete Employee class implementation

--- src/main/java/com/pluralsight/generics/Employee.java
+++ src/main/java/com/pluralsight/generics/Employee.java
@@ -1,4 +1,12 @@
 package com.pluralsight.generics;
 
-public class Employee {
+public class Employee extends Person {
+    public Employee(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{name='" + getName() + "', age=" + getAge() + "}";
+    }
 }
