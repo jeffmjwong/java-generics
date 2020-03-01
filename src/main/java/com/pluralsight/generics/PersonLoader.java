6cb9b8151605018e7db882be86beadbdeb5f0f81
Jeff Wong <v-jwong1@expedia.com>
2020-03-01 17:03:38 +1000

    complete PersonLoader class

--- src/main/java/com/pluralsight/generics/PersonLoader.java
+++ src/main/java/com/pluralsight/generics/PersonLoader.java
@@ -1,4 +1,17 @@
 package com.pluralsight.generics;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+
 public class PersonLoader {
+    private final RandomAccessFile file;
+
+    public PersonLoader(final File file) throws FileNotFoundException {
+        this.file = new RandomAccessFile(file, "rw");
+    }
+
+    public Person load() throws ClassNotFoundException {
+
+    }
 }
