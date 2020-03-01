98054c0f734581fe7d273b0049a429d8b2ecc256
Jeff Wong <v-jwong1@expedia.com>
2020-03-01 12:11:43 +1000

    create constructor for SortedPair class that takes 2 arguments

--- src/main/java/com/pluralsight/generics/SortedPair.java
+++ src/main/java/com/pluralsight/generics/SortedPair.java
@@ -3,4 +3,9 @@ package com.pluralsight.generics;
 public class SortedPair<T> {
     private T first;
     private T second;
+
+    public SortedPair(T t1, T t2) {
+        first = t1;
+        second = t2;
+    }
 }
