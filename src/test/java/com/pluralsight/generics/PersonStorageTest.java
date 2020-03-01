6327d13e9a930eb9ebe0001462f4534a38e5bb22
Jeff Wong <v-jwong1@expedia.com>
2020-03-01 17:50:21 +1000

    create savesAndLoadsPerson test case

--- src/test/java/com/pluralsight/generics/PersonStorageTest.java
+++ src/test/java/com/pluralsight/generics/PersonStorageTest.java
@@ -1,4 +1,26 @@
 package com.pluralsight.generics;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
 public class PersonStorageTest {
+    private Partner matt = new Partner("Matt", 57);
+    private Partner nick = new Partner("Nick", 53);
+    private Employee mike = new Employee("Mike", 39);
+
+    private File file;
+    private PersonSaver saver;
+    private PersonLoader loader;
+
+    @Test
+    public void savesAndLoadPerson() throws Exception {
+        Person person = new Person("Bob", 20);
+        saver.save(matt);
+        saver.save(mike);
+
+        assertEquals(person, loader.load());
+    }
 }
