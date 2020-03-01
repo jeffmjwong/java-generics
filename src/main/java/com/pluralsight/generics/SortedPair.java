0f28806d869f4fe1f819bdec1da3417b8836c660
Jeff Wong <v-jwong1@expedia.com>
2020-03-01 12:13:10 +1000

    set up getters

--- src/main/java/com/pluralsight/generics/SortedPair.java
+++ src/main/java/com/pluralsight/generics/SortedPair.java
@@ -1,11 +1,18 @@
 package com.pluralsight.generics;
 
 public class SortedPair<T> {
-    private T first;
-    private T second;
+    private final T first;
+    private final T second;
 
     public SortedPair(T t1, T t2) {
         first = t1;
         second = t2;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+    public T getSecond() {
+        return second;
     }
 }
