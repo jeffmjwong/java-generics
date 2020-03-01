73c366f1f243c7bc74c02ac3a723aa0707bae70c
Jeff Wong <v-jwong1@expedia.com>
2020-03-01 15:51:36 +1000

    clean up Main class

--- src/main/java/com/pluralsight/generics/Main.java
+++ src/main/java/com/pluralsight/generics/Main.java
@@ -2,11 +2,8 @@ package com.pluralsight.generics;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
