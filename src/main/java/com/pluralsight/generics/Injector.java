b414c0df55a861399deb82320bee04fbc69a8d86
Jeff Wong <v-jwong1@expedia.com>
2020-03-04 10:09:33 +1000

    complete Injector class

--- src/main/java/com/pluralsight/generics/Injector.java
+++ src/main/java/com/pluralsight/generics/Injector.java
@@ -1,4 +1,17 @@
 package com.pluralsight.generics;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Injector {
+    private Map<Class<?>, Object> objectGraph = new HashMap<>();
+
+    public Injector with(final Object value) {
+        objectGraph.put(value.getClass(), value);
+        return this;
+    }
+
+    public Object newInstance(final String className) {
+        return null;
+    }
 }
